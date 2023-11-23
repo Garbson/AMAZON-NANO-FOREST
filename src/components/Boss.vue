a7b6fe75f178fc5478aee5d368316606fffaf8e9
leonardo-cordeiro <gama05596@gmail.com>
2023-11-22 21:22:47 -0300

    Adicionei nav-bar, contact section mudei as cores

--- src/components/Boss.vue
+++ src/components/Boss.vue
@@ -1,77 +1,70 @@
 <template>
-    <section class="flex q-pa-lg bg-dark justify-center">
-      <div class="q-pa-lg">
-        <h1 class="text-h4 text-white text-raleway q-pt-xl font-serif">
-          Fernando Drummond
-        </h1>
-        <p class="qualificações size-18 text-montserrat-400 text-white">
-          Engenheiro Químico; <br />Doutor em Biotecnologia <br />Pós-Doutor em
-          Nanobiotecnologia <br />Diretor Chefe de Tecnologia e Founder da
-          Amazon NanoForest
-        </p>
-      </div>
+  <section class="flex q-pa-lg bg-white justify-center" id="equipe">
+    <div class="q-pa-lg">
+      <h1 class="text-h4 text-black text-raleway q-pt-xl font-serif">Fernando Drummond</h1>
+      <p class="qualificações size-18 text-montserrat-400 text-black">
+        Engenheiro Químico; <br />Doutor em Biotecnologia <br />Pós-Doutor em Nanobiotecnologia <br />Diretor Chefe de Tecnologia e Founder
+        da Amazon NanoForest
+      </p>
+    </div>
 
-      <div class="q-pa-md q-mt-lg">
-        <img class="equipe" src="img/Fernando.jpg" alt="" />
-      </div>
-    </section>
+    <div class="q-pa-md q-mt-lg">
+      <img class="equipe" src="img/Fernando.jpg" alt="" />
+    </div>
+  </section>
 
-    <section class="aaa flex q-pa-lg justify-center text-left">
-      <div class="q-pa-lg text-right">
-        <h1 class="text-h4 text-raleway q-pt-xl text-left font-serif">Kauã Macedo</h1>
-        <p class="qualificações size-18 text-montserrat-400 text-left">
-          Graduando em Farmácia <br />
-          e Sistemas da Informação <br />
-          CEO da Startup Nano Fruit <br />
-          Vice-Diretor de produção e Co-Founder da Amazon NanoForest
-        </p>
-      </div>
-      <div class="q-pa-md q-mt-lg">
-        <img class="equipe" src="img/kaua.jpg" alt="" />
-      </div>
-    </section>
+  <section class="aaa flex q-pa-lg bg-green-800 justify-center text-left">
+    <div class="q-pa-lg text-right text-white">
+      <h1 class="text-h4 text-raleway q-pt-xl text-left font-serif">Kauã Macedo</h1>
+      <p class="qualificações size-18 text-montserrat-400 text-left">
+        Graduando em Farmácia <br />
+        e Sistemas da Informação <br />
+        CEO da Startup Nano Fruit <br />
+        Vice-Diretor de produção e Co-Founder da Amazon NanoForest
+      </p>
+    </div>
+    <div class="q-pa-md q-mt-lg">
+      <img class="equipe" src="img/kaua.jpg" alt="" />
+    </div>
+  </section>
 
-    <section class="flex q-pa-lg bg-dark justify-center font-serif">
-      <div class="q-pa-lg">
-        <h1 class="text-h4 text-white text-raleway q-pt-xl font-serif">Joana Maia</h1>
-        <p class="qualificações size-18 text-montserrat-400 text-white">
-          Graduanda em Nutrição <br />Chefe de Comunicação e Co-founder da
-          Amazon NanoForest
-        </p>
-      </div>
+  <section class="flex q-pa-lg bg-white justify-center font-serif">
+    <div class="q-pa-lg text-black">
+      <h1 class="text-h4 text-raleway q-pt-xl font-serif">Joana Maia</h1>
+      <p class="qualificações size-18 text-montserrat-400">
+        Graduanda em Nutrição <br />Chefe de Comunicação e Co-founder da Amazon NanoForest
+      </p>
+    </div>
 
-      <div class="q-pa-md q-mt-lg">
-        <img class="equipe" src="img/joana.jpg" alt="" />
-      </div>
-    </section>
+    <div class="q-pa-md q-mt-lg">
+      <img class="equipe" src="img/joana.jpg" alt="" />
+    </div>
+  </section>
 
-    <section class="aaa flex q-pa-lg justify-center text-left font-serif">
-      <div class="q-pa-lg text-right">
-        <h1 class="text-h4 text-raleway q-pt-xl text-left font-serif">Bruna Braga</h1>
-        <p class="qualificações size-18 text-montserrat-400 text-left">
-          Graduanda em Farmácia <br />CEO da Startup Nanocaphair <br />Técnica
-          em Biotecnologia e Co-Founder da Amazon NanoForest
-        </p>
-      </div>
-      <div class="q-pa-md q-mt-lg">
-        <img class="equipe" src="img/Bruna.jpg" alt="" />
-      </div>
-    </section>
+  <section class="aaa flex q-pa-lg bg-green-800 justify-center text-left font-serif">
+    <div class="q-pa-lg text-right text-white">
+      <h1 class="text-h4 text-raleway q-pt-xl text-left font-serif">Bruna Braga</h1>
+      <p class="qualificações size-18 text-montserrat-400 text-left">
+        Graduanda em Farmácia <br />CEO da Startup Nanocaphair <br />Técnica em Biotecnologia e Co-Founder da Amazon NanoForest
+      </p>
+    </div>
+    <div class="q-pa-md q-mt-lg">
+      <img class="equipe" src="img/Bruna.jpg" alt="" />
+    </div>
+  </section>
 
-    <section class="flex q-pa-lg bg-dark justify-center">
-      <div class="q-pa-lg">
-        <h1 class="text-h4 text-white text-raleway q-pt-xl font-serif">Iago Pimenta</h1>
-        <p class="qualificações size-18 text-montserrat-400 text-white">
-          Chefe de Designer <br />CEO da Startup Gold Nanoserum e Co-Founder da
-          Amazon NanoForest
-        </p>
-      </div>
+  <section class="flex q-pa-lg bg-white justify-center">
+    <div class="q-pa-lg text-black">
+      <h1 class="text-h4 text-raleway q-pt-xl font-serif">Iago Pimenta</h1>
+      <p class="qualificações size-18 text-montserrat-400">
+        Chefe de Designer <br />CEO da Startup Gold Nanoserum e Co-Founder da Amazon NanoForest
+      </p>
+    </div>
 
-      <div class="q-pa-md q-mt-lg">
-        <img class="equipe" src="img/iago.jpg" alt="" />
-      </div>
-    </section>
-
+    <div class="q-pa-md q-mt-lg">
+      <img class="equipe" src="img/iago.jpg" alt="" />
+    </div>
+  </section>
 </template>
 <style scoped>
 .equipe {
@@ -82,7 +75,7 @@
 }
 
 .qualificações {
-  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+  font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
   font-size: x-large;
 }
 .aaa {
@@ -92,5 +85,4 @@
     flex-direction: rom;
   }
 }
-
 </style>
